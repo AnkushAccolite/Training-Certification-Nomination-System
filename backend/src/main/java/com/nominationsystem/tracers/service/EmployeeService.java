a38f3610f39604580aea2de26aa65a59ade264a4
Ankush <kumar.ankush1@accolitedigital.com>
2024-05-08 23:49:10 +0530

    add: employee and auth related services

--- backend/src/main/java/com/nominationsystem/tracers/service/EmployeeService.java
+++ backend/src/main/java/com/nominationsystem/tracers/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.nominationsystem.tracers.service;
 
 import com.nominationsystem.tracers.models.Employee;
+import com.nominationsystem.tracers.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -8,12 +9,20 @@ import java.util.List;
 @Service
 public class EmployeeService {
 
-//    @Autowired
-//    private EmployeeRepository employeeRepository;
-//
-//    public List<Employee> getAllEmployees() {
-//        return employeeRepository.findAll();
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+//    public Employee addEmployee(Employee employee) {
+//        String hashedPassword = utilityServices.encodePassword(employee.getHashPswd());
+//        employee.setHashPswd(hashedPassword);
+//        Employee save = this.employeeRepository.save(employee);
+//        return save;
 //    }
+
+    public List<Employee> getAllEmployees(){
+        return this.employeeRepository.findAll();
+    }
 
     // Other service methods for CRUD operations
 }
