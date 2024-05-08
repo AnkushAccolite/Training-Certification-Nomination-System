543431be697124511a78fdcc35649457b881f291
Ankush <kumar.ankush1@accolitedigital.com>
2024-05-08 23:46:12 +0530

    add: Login and Signup requests

---
+++ backend/src/main/java/com/nominationsystem/tracers/payload/request/SignupRequest.java
@@ -0,0 +1,55 @@
+package com.nominationsystem.tracers.payload.request;
+
+import jakarta.validation.constraints.*;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private String role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+
+        this.username = username;
+        this.email=username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
