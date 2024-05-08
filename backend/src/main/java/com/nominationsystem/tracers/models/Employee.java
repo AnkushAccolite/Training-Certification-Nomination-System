7257f2b2f6d056813d8e6af96fd77bc50ddd6cd5
Ankush <kumar.ankush1@accolitedigital.com>
2024-05-08 23:43:18 +0530

    add: course and employee model

--- backend/src/main/java/com/nominationsystem/tracers/models/Employee.java
+++ backend/src/main/java/com/nominationsystem/tracers/models/Employee.java
@@ -1,30 +1,38 @@
 package com.nominationsystem.tracers.models;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 @Getter
 @Setter
-@Document(collection = "Employee")
+@Document(collection = "employees")
 public class Employee {
-
     @Id
+    private String id;
+
     private String empId;
 
     @NotBlank
-    private String name;
+    @Size(max = 20)
+    private String username;
 
+    @NotBlank
+    @Size(max = 50)
     @Email
     private String email;
 
     @NotBlank
-    private String hashPswd;
+    @Size(max = 120)
+    private String password;
+
 
     private String role;
 
@@ -37,4 +45,48 @@ public class Employee {
     private Boolean eligibleForPromotions;
 
     private String managerId;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        this.email=username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
 }
