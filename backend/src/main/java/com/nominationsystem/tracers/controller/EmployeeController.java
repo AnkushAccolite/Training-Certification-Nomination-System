db1fe862dd11f728175bda0fcd27a79ef1b9d3ed
Ankush <kumar.ankush1@accolitedigital.com>
2024-05-09 14:20:13 +0530

    add: Employee routes

--- backend/src/main/java/com/nominationsystem/tracers/controller/EmployeeController.java
+++ backend/src/main/java/com/nominationsystem/tracers/controller/EmployeeController.java
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -25,4 +30,25 @@ public class EmployeeController {
     public ResponseEntity<?> getEmployees() {
         return ResponseEntity.ok(this.employeeService.getAllEmployees());
     }
+
+    @PostMapping("/getByEmail")
+    public ResponseEntity<?> getEmployeeByEmail(@RequestBody Map<String, String> requestBody) {
+        return this.employeeService.getEmpByEmail(requestBody);
+    }
+
+//    @PatchMapping("/updateIsAdmin")
+//    public ResponseEntity<?> updateIsAdmin(@RequestParam String email, @RequestParam boolean isAdmin) {
+//        return this.employeeService.setAdmin(email,isAdmin);
+//    }
+    @PatchMapping("/setRole")
+    public ResponseEntity<?> setRole(@RequestParam String email, @RequestParam String role) {
+        return this.employeeService.setRole(email,role);
+    }
+
+    @PatchMapping("/addCourses")
+    public ResponseEntity<?> addCoursesToEmployee(@RequestParam String email, @RequestParam String courseIds) {
+        return this.employeeService.addCourses(email,courseIds);
+    }
+
+
 }
