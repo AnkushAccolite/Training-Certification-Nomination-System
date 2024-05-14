c9cfe3688a9ea26c36393f384de8dc015e44cafe
Ankush <kumar.ankush1@accolitedigital.com>
2024-05-15 01:20:04 +0530

    add: findByCourseId

--- backend/src/main/java/com/nominationsystem/tracers/repository/CourseRepository.java
+++ backend/src/main/java/com/nominationsystem/tracers/repository/CourseRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface CourseRepository extends MongoRepository<Course, String> {
 
     Course findByCourseName(String courseName);
+    Course findByCourseId(String courseId);
 }
