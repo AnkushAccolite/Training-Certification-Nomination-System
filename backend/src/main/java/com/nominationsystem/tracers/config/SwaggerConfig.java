01db70f03dd1ce2a3c92a4816c30959d8e98dc05
Debayan Das <debayan.das@accolitedigital.com>
2024-05-22 14:53:49 +0530

    added swagger

---
+++ backend/src/main/java/com/nominationsystem/tracers/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.nominationsystem.tracers.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
