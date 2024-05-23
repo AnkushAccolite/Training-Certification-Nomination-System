d94bbafaae8e91c6f22923ca1f8f891abf6e3037
prasanna-papulla <papulla.prasanna@accolitedigital.com>
2024-05-23 17:20:55 +0530

    aligned heading

--- frontend/src/views/pages/CourseFAQ.jsx
+++ frontend/src/views/pages/CourseFAQ.jsx
@@ -54,9 +54,7 @@ function CourseFAQ() {
 
   return (
     <div style={{ padding: '8px' }}>
-      <Typography variant="h3" gutterBottom sx={{ marginBottom: '20px' }}>
-        Course FAQ's
-      </Typography>
+      <h2 style={{textAlign:'center', paddingBottom:'20px'}}>Course FAQ's</h2>
       {faqs.map((faq, index) => (
         <Accordion
           key={index}
