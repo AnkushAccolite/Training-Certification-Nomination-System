5fa2bef8d1dfd3808dc010326e5f7b30bda7186f
Ankush <kumar.ankush1@accolitedigital.com>
2024-05-27 17:45:59 +0530

    dependecy conflict fixes

--- frontend/src/views/pages/authentication3/Register3.jsx
+++ frontend/src/views/pages/authentication3/Register3.jsx
@@ -10,7 +10,6 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 // project imports
 import AuthWrapper1 from '../AuthWrapper1';
 import AuthCardWrapper from '../AuthCardWrapper';
-import Logo from 'ui-component/Logo';
 import AuthRegister from '../authentication/auth-forms/AuthRegister';
 import AuthFooter from 'ui-component/cards/AuthFooter';
 
@@ -31,7 +30,8 @@ const Register = () => {
                 <Grid container spacing={2} alignItems="center" justifyContent="center">
                   <Grid item sx={{ mb: 3 }}>
                     <Link to="#" aria-label="theme logo">
-                      <Logo />
+                      {/* <Logo /> */}
+                      Teknow
                     </Link>
                   </Grid>
                   <Grid item xs={12}>
